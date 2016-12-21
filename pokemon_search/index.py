3de2839bbe1a65638452651ed1a4843fd13141a4
random-residual <dtsukiyama@workmarket.com>
2016-12-20 22:23:49 -0500

    update index.py

--- pokemon_search/index.py
+++ pokemon_search/index.py
@@ -2,28 +2,28 @@
 import json
 import requests   
         
-def reindex(analysis_settings={}, mapping_settings={}, Dict={}, index_name = "index_name", doc_type = "doc_type", identifier = "identifier"):
+def reindex(analysisSettings={}, mappingSettings={}, Dict={}, index_name = "index_name", doc_type = "doc_type", identifier = "identifier"):
     settings = { 
-            "settings": {
-               "number_of_shards": 1, 
-               "index": {
-                    "analysis" : analysis_settings, 
-                }}}
+        "settings": {
+            "number_of_shards": 1, 
+            "index": {
+                "analysis" : analysisSettings, 
+            }}}
 
-    if mapping_settings:
-        settings['mappings'] = mapping_settings 
+    if mappingSettings:
+        settings['mappings'] = mappingSettings 
 
     resp = requests.delete("http://localhost:9200/" + str(index_name)) 
-    resp = requests.put("http://localhost:9200/" + str(index_name), data=json.dumps(settings))
+    resp = requests.put("http://localhost:9200/" + str(index_name), 
+                        data=json.dumps(settings))
 
     bulk_upload = ""
     print "building..."
     for id, data in Dict.iteritems(): 
-         addCmd = {"index": {"_index": str(index_name), 
-                             "_type": str(doc_type),
-                             "_id": data[str(identifier)]}}
-    bulk_upload += json.dumps(addCmd) + "\n" + json.dumps(data) + "\n"
+        addCmd = {"index": {"_index": str(index_name), 
+                            "_type": str(doc_type),
+                            "_id": data[str(identifier)]}}
+        bulk_upload += json.dumps(addCmd) + "\n" + json.dumps(data) + "\n"
 
     print "indexing..."
     resp = requests.post("http://localhost:9200/_bulk", data=bulk_upload)
-            
